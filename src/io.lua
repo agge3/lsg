57dd7c49a0ea2d1ab5ab645603dc3fc8295423c7
agge3 <sterspark@gmail.com>
2024-06-08 06:06:53 -0700

    Added class bsdio To wrapper standard lua assert(fh:write()) as a simpler call To package common FreeBSD syscall io procedures as a reusable interface To (maybe) have different behavior based on environment variables Did not remove generated_tag() from util, or change call in modules To avoid breakage until confirmed to be a good approach Sponsored by:    Google (GSoC 2024)

---
+++ src/io.lua
@@ -0,0 +1,88 @@
+--
+-- SPDX-License-Identifier: BSD-2-Clause
+--
+-- Copyright (c) 2024 Tyler Baxter <agge@FreeBSD.org>
+--
+--
+-- Thanks to Kyle Evans for his makesyscall.lua in FreeBSD which served as
+-- inspiration for this, and as a source of code at times.
+--
+-- SPDX-License-Identifier: BSD-2-Clause-FreeBSD
+--
+-- Copyright (c) 2019 Kyle Evans <kevans@FreeBSD.org>
+--
+
+config = require("config")
+util = require("util")
+
+local bsdio = {}
+
+bsdio.__index = bsdio
+
+function bsdio:write(line)
+	assert(self.fh:write(line))
+end
+
+function bsdio:pad64(bool)
+    if bool then
+	    self:write(string.format([[
+#if !defined(PAD64_REQUIRED) && !defined(__amd64__)
+#define PAD64_REQUIRED
+#endif
+]]))
+    end
+end
+
+-- Writes the generated tag.
+-- @param str
+-- The title of the file.
+-- @param [comment]
+-- Default comment is C comments. Optional to change comment (e.g., to sh 
+-- comments). Will still follow C-style indentation. @see style(9)
+-- @note Handles multi-line titles, deliminated by newlines.
+function bsdio:generated_tag(str, comment)
+    local comment_start = comment or "/*"
+    local comment_middle = comment or "*"
+    local comment_end = comment or "*/"
+    local tag = "@" .. "generated"
+
+    -- Don't enter loop if it's the simple case.
+    if str:find("\n") == nil then
+        self:write(string.format([[%s
+ %s %s
+ %s
+ %s DO NOT EDIT-- this file is automatically %s.
+ %s
+]], comment_start, comment_middle, str, comment_middle, comment_middle, tag, 
+            comment_end)) 
+
+    else
+        self:write(string.format([[%s]], comment_start))
+        for line in str:gmatch("[^\n]*") do
+            if line ~= nil then
+                self:write(string.format([[
+ %s %s]], comment_middle, line))
+            end
+        end
+        self:write(string.format([[ %s
+ %s DO NOT EDIT-- this file is automatically %s
+ %s
+]], comment_middle, comment_middle, tag, comment_end))
+    end
+end
+
+function bsdio:new(obj, fh)
+    obj = obj or { }
+    setmetatable(obj, self)
+    self.__index == self
+
+    self.bsdio = fh
+
+    if self.fh == nil then
+        util.abort("Not found: " .. self.fh)
+    end
+
+    return obj
+end
+
+return bsdio
--- src/sysproto_h.lua
+++ src/sysproto_h.lua
@@ -23,3 +23,45 @@ local cfg = {
 local function gen_sysproto_h(tbl, cfg)
     util.generated_tag("System call prototypes.")
 end
+
+-- noncompat
+local nargflags = get_mask({"NOARGS", "NOPROTO", "NODEF"})
+	if flags & nargflags == 0 then
+	if #funcargs > 0 then
+			write_line("sysarg", string.format("struct %s {\n",
+			    argalias))
+			for _, v in ipairs(funcargs) do
+				local argname, argtype = v.name, v.type
+				if argtype == "int" and argname == "_pad" and abi_changes("pair_64bit") then
+					write_line("sysarg", "#ifdef PAD64_REQUIRED\n")
+				end
+				write_line("sysarg", string.format(
+				    "\tchar %s_l_[PADL_(%s)]; %s %s; char %s_r_[PADR_(%s)];\n",
+				    argname, argtype,
+				    argtype, argname,
+				    argname, argtype))
+				if argtype == "int" and argname == "_pad" and abi_changes("pair_64bit") then
+					write_line("sysarg", "#endif\n")
+				end
+			end
+			write_line("sysarg", "};\n")
+		else
+			write_line("sysarg", string.format(
+			    "struct %s {\n\tsyscallarg_t dummy;\n};\n", argalias))
+		end
+	end
+	local protoflags = get_mask({"NOPROTO", "NODEF"})
+	if flags & protoflags == 0 then
+		local sys_prefix = "sys_"
+		if funcname == "nosys" or funcname == "lkmnosys" or
+		    funcname == "sysarch" or funcname:find("^freebsd") or
+		    funcname:find("^linux") then
+			sys_prefix = ""
+		end
+		write_line("sysdcl", string.format(
+		    "%s\t%s%s(struct thread *, struct %s *);\n",
+		    rettype, sys_prefix, funcname, argalias))
+		write_line("sysaue", string.format("#define\t%sAUE_%s\t%s\n",
+		    config.syscallprefix, funcalias, auditev))
+	end
+
